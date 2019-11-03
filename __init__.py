6b05f7c0e4ea3c63a3981a03666be848a4dbd3df
progrunner17 <g.sho1500@gmail.com>
2019-11-03 12:29:49 +0900

    大会記録表示の整形

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://honomara:honomara@localhost/honomara'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.jinja_env.globals.update(len=len)
+app.jinja_env.globals.update(len=len, int=int)
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
