f2205d0895fca45e36d2602770736d21849ee323
progrunner17 <g.sho1500@gmail.com>
2020-08-20 09:25:23 +0900

    各種編集＆削除リンク（アイコン）の追加＆修正

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {config, dom, library} from '@fortawesome/fontawesome-svg-core';
-import {faEdit} from '@fortawesome/free-solid-svg-icons';
+import {faPen,faEdit,faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 library.add(faEdit);
+library.add(faPen);
+library.add(faTrashAlt);
 
 dom.i2svg();
